b030ee84ee30d79edd48d1a8c2330440e131e4d6
AaronPiramide <152392618+AaronPiramide@users.noreply.github.com>
2025-12-17 08:50:42 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <script src='validacion.js'></script>
+</head>
+
+<body>
+
+
+
+    <main>
+        <?php
+        <h1>Prueba github actions</h1>
+
+        echo "Que dice el tio"
+    >
+    </main>
+  
+    </body>
+
+    </html>
